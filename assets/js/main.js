0f76f992cbdac4504b1771e04387527e27482629
tovarsz2 <73807873+tovarsz2@users.noreply.github.com>
2024-10-08 17:21:04 -0600

    fix script

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,7 @@
 let menuIcon = document.querySelector('#menu-icon');
 let navbar = document.querySelector('.navbar');
 let sections = document.querySelectorAll('section');
-let navLinks = document.querySelectorAll('header nav a')
+let navLinks = document.querySelectorAll('header nav a');
 
 window.onscroll = () => {
 	sections.forEach(sec => {
